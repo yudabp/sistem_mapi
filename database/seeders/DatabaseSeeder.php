0cbf6a14f33db6d9316bf39b05d188cca89e580b
yudabp <tama.yuda48@gmail.com>
2025-10-14 12:47:27 +0700

    feat: add role column to users table and update UserFactory and UserSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            UserSeeder::class,
             // DasboardTableSeeder::class,
             EmployeesTableSeeder::class,
             ProductionTableSeeder::class,
